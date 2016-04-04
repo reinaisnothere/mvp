535cbb2340b352d0c82240aa43c3588228e78788
Reina Mei <reinameic@gmail.com>
2016-04-04 12:56:56 -0700

    Simplify returned data for display & db insertion

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -3,8 +3,26 @@ angular.module('cat-buddy.services', [])
   var getAllFromInternetByZipCode = function(animalName, zipCode, count, offset, scope) {
     offset = offset || 0;
     return $http.jsonp('http://api.petfinder.com/pet.find?' + 'key=' + PETFINDER_API_KEY + '&animal=' + animalName + '&location=' + zipCode + '&count=' + count + '&format=json' + '&offset=' + offset + '&callback=JSON_CALLBACK')
-    .success(function(resp){
-      scope.data.cats = resp.petfinder.pets.pet;
+    .success(function(resp) {
+      var cats = resp.petfinder.pets.pet;
+      scope.data.cats = [];
+      for (var i = 0; i < cats.length; i++) {
+        var cat = cats[i];
+        if (cat.media.photos && cat.contact) {
+          var simplifiedCat = {
+            id: cat.id['$t'],
+            name: cat.name['$t'],
+            age: cat.age['$t'],
+            description: cat.description['$t'],
+            photo: cat.media.photos.photo[3]['$t'],
+            'contact-name': cat.contact.name ? cat.contact.name['$t'] : '',
+            phone: cat.contact.phone ? cat.contact.phone['$t'] : '',
+            email: cat.contact.email ? cat.contact.email['$t'] : '',
+            zip: cat.contact.zip['$t']
+          };
+          scope.data.cats.push(simplifiedCat);
+        }
+      }
     })
     .error(function(err) {
       console.error.bind(console, 'Failed to get cats');
@@ -32,5 +50,3 @@ angular.module('cat-buddy.services', [])
     addOneToSaved: addOneToSaved
   };
 });
-
-// return $http.jsonp('http://api.petfinder.com/pet.find?' + 'key=' + PETFINDER_API_KEY + '&animal=' + animalName + '&count=' + count + '&offset=' + offset + '&loation=' + zipCode + '&format=json&callback=JSON_CALLBACK')
