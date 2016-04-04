1be7a09740edf24ee7e13a1ad405cf00068cc1d2
Reina Mei <reinameic@gmail.com>
2016-04-04 15:53:17 -0700

    Clear $scope.cats for new search

--- client/app/search/cat/searchCats.js
+++ client/app/search/cat/searchCats.js
@@ -1,10 +1,15 @@
 angular.module('cat-buddy.search-cats', [])
 
 .controller('SearchCatsController', function ($scope, Animals) {
-
   $scope.cats = [];
   var offset = 0;
   var count = 10;
+
+  $scope.newFindCatsByZipCode = function() {
+    $scope.cats = [];
+    Animals.getAllFromInternetByZipCode('cat', $scope.zipCode, count, 0, $scope);
+    offset += count;
+  };
 
   $scope.findCatsByZipCode = function() {
     Animals.getAllFromInternetByZipCode('cat', $scope.zipCode, count, offset, $scope);
