e2e16638a3adf210fd61bfe6176377f37412bc23
Reina Mei <reinameic@gmail.com>
2016-04-04 12:15:04 -0700

    Use $http.jsonp to bypass same origin policy

--- client/app/search/cat/searchCats.js
+++ client/app/search/cat/searchCats.js
@@ -1,19 +1,12 @@
 angular.module('cat-buddy.search-cats', [])
 
 .controller('SearchCatsController', function ($scope, Animals) {
-  // Your code here
 
   $scope.data = {};
   var offset = 0;
 
   $scope.findCatsByZipCode = function() {
-    Animals.getAllFromInternetByZipCode('cat', $scope.zipCode, 20, offset)
-      .then(function(cats) {
-        $scope.data.cats = cats;
-      })
-      .catch(function(error) {
-        console.error.bind(console, error);
-      });
+    Animals.getAllFromInternetByZipCode('cat', $scope.zipCode, 20, offset, $scope);
     offset += 20;
   };
 
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,21 +1,13 @@
 angular.module('cat-buddy.services', [])
 .factory('Animals', function($http) {
-  var getAllFromInternetByZipCode = function(animalName, zipCode, count, offset) {
+  var getAllFromInternetByZipCode = function(animalName, zipCode, count, offset, scope) {
     offset = offset || 0;
-    return $http({
-      method: 'GET',
-      url: 'http://api.petfinder.com/pet.find',
-      data: {
-        key: PETFINDER_API_KEY,
-        animal: animalName,
-        count: count,
-        offset: offset,
-        location: zipCode,
-        format: 'json'
-      }
+    return $http.jsonp('http://api.petfinder.com/pet.find?' + 'key=' + PETFINDER_API_KEY + '&animal=' + animalName + '&location=' + zipCode + '&count=' + count + '&format=json' + '&offset=' + offset + '&callback=JSON_CALLBACK')
+    .success(function(resp){
+      scope.data.cats = resp.petfinder.pets.pet;
     })
-    .then(function(resp){
-      return resp.petfinder.pets;
+    .error(function(err) {
+      console.error.bind(console, 'Failed to get cats');
     });
   };
 
@@ -40,3 +32,5 @@ angular.module('cat-buddy.services', [])
     addOneToSaved: addOneToSaved
   };
 });
+
+// return $http.jsonp('http://api.petfinder.com/pet.find?' + 'key=' + PETFINDER_API_KEY + '&animal=' + animalName + '&count=' + count + '&offset=' + offset + '&loation=' + zipCode + '&format=json&callback=JSON_CALLBACK')
